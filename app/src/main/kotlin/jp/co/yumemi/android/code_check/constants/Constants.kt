91aa17ffe5689aac87db1130b414e73337726da3
Hasitha-Su <hasithakms@gmail.com>
2023-06-17 12:52:04 +0530

    Add constants file and update with GitHub API Url

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/constants/Constants.kt
@@ -0,0 +1,11 @@
+package jp.co.yumemi.android.code_check.constants
+
+/**
+ * Constants used in the application.
+ * Includes base URL, paths, and endpoint definitions for the GitHub API.
+ */
+object Constants {
+    const val BASE_URL = "https://api.github.com/"
+    const val PATH_SEARCH = "search/"
+    const val ENDPOINT_REPOSITORIES = "repositories"
+}
